63da204507bd3b83e52c793aa483b8b686f47ba4
Rhianon Kilzer <rhianonkilzer@yahoo.com>
2018-08-29 15:50:03 -0600

    player cards

--- src/components/PlayerHand.vue
+++ src/components/PlayerHand.vue
@@ -1,37 +1,46 @@
 <template>
     <div class="player-hand">
-<div>
-<h1>{{playerData.name || ''}}</h1>
-</div>
+        <div>
+            <h1>
+                {{playerData.name || ''}}
+            </h1>
+        </div>
+        <div class="row">
+            <div class="col-3 card" v-for="playerCard in playerData.hand":key="playerCard.id">
+                <h1>{{playerCard.name}}</h1>
+                <img :src="playerCard.img" alt="">
+            </div>
+        </div>
     </div>
 </template>
 
 
 <script>
-export default {
-    name: "PlayerHand",
-    data() {
-        return {}
-    },
-    computed: {
-        playerData(){
-            if(this.$store.state.game.id){
-                return this.$store.state.game.players[1]
+    export default {
+        name: "PlayerHand",
+        data() {
+            return {}
+        },
+        computed: {
+            playerData() {
+                if (this.$store.state.game.id) {
+                    return this.$store.state.game.players[1]
+                }
+                return {}
             }
-            return{}
-        }
-    },
-    methods: {
+        },
+        methods: {
 
+        }
     }
-}
 </script>
 <style scoped>
-    h1{
+    h1 {
         font-size: 1.8rem;
         color: white;
     }
-    .player-hand{
+
+    .player-hand {
         font-family: 'Nova Mono', monospace;
     }
 </style>
